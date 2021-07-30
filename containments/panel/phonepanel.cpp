c122b19a562b2b1e3ca403ad123ebb11d135c6b8
Tobias Fella <fella@posteo.de>
2021-07-30 16:19:46 +0200

    Port signal strength indicator to ModemManager

--- containments/panel/phonepanel.cpp
+++ containments/panel/phonepanel.cpp
@@ -25,6 +25,9 @@
 #include <QScreen>
 #include <QStandardPaths>
 #include <QtConcurrent/QtConcurrent>
+#include <QQmlEngine>
+
+#include "signalindicator.h"
 
 #define FORMAT24H "HH:mm:ss"
 
@@ -94,6 +97,8 @@ PhonePanel::PhonePanel(QObject *parent, const QVariantList &args)
             Q_EMIT isSystem24HourFormatChanged();
         }
     });
+
+    qmlRegisterSingletonInstance("org.kde.plasma.phone", 1, 0, "SignalIndicator", new SignalIndicator());
 }
 
 PhonePanel::~PhonePanel() = default;
