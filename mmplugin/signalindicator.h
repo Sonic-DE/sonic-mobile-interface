d7dcf8bba52afb24865ccdae31e52a862c0db3ae
Tobias Fella <fella@posteo.de>
2021-09-10 13:00:50 +0200

    another test

--- mmplugin/signalindicator.h
+++ mmplugin/signalindicator.h
@@ -3,8 +3,9 @@
 
 #pragma once
 
-#include <QObject>
 #include <ModemManagerQt/Manager>
+#include <ModemManagerQt/modem3gpp.h>
+#include <QObject>
 
 class SignalIndicator : public QObject
 {
@@ -13,6 +14,7 @@ class SignalIndicator : public QObject
     Q_PROPERTY(int strength READ strength NOTIFY strengthChanged)
     Q_PROPERTY(QString name READ name NOTIFY nameChanged)
     Q_PROPERTY(bool simLocked READ simLocked NOTIFY simLockedChanged)
+    Q_PROPERTY(bool available READ available NOTIFY availableChanged)
 
 public:
     SignalIndicator();
@@ -20,12 +22,16 @@ public:
     int strength() const;
     QString name() const;
     bool simLocked() const;
+    bool available() const;
 
 Q_SIGNALS:
     void strengthChanged();
     void nameChanged();
     void simLockedChanged();
+    void availableChanged();
 
 private:
     ModemManager::Modem::Ptr m_modem;
+    ModemManager::Modem3gpp::Ptr m_3gppModem;
+    void updateModem();
 };
