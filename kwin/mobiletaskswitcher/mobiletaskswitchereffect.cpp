60d641bc03934dd80b5a4f35359dd5de794314cd
Devin Lin <espidev@gmail.com>
2023-02-22 08:51:29 -0800

    Update plugin

--- kwin/mobiletaskswitcher/mobiletaskswitchereffect.cpp
+++ kwin/mobiletaskswitcher/mobiletaskswitchereffect.cpp
@@ -1,18 +1,21 @@
 // SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
-// SPDX-FileCopyrightText: 2022 Devin Lin <devin@kde.org>
+// SPDX-FileCopyrightText: 2023 Devin Lin <devin@kde.org>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "mobiletaskswitchereffect.h"
 
 #include <QKeyEvent>
-#include <QTimer>
+#include <QMetaObject>
+#include <QQuickItem>
 
 namespace KWin
 {
 
 MobileTaskSwitcherEffect::MobileTaskSwitcherEffect()
+    : m_shutdownTimer(new QTimer(this))
 {
-    qDebug() << "INITIALIZE MOBILE TASK SWITCHER";
+    m_shutdownTimer->setSingleShot(true);
+    connect(m_shutdownTimer, &QTimer::timeout, this, &MobileTaskSwitcherEffect::realDeactivate);
 
     const QKeySequence defaultToggleShortcut = Qt::META | Qt::Key_C;
 
@@ -25,7 +28,42 @@ MobileTaskSwitcherEffect::MobileTaskSwitcherEffect()
     KGlobalAccel::self()->setDefaultShortcut(m_toggleAction, {defaultToggleShortcut});
     KGlobalAccel::self()->setShortcut(m_toggleAction, {defaultToggleShortcut});
 
-    //     connect(effects, &EffectsHandler::screenAboutToLock, this, &MobileTaskSwitcherEffect::realDeactivate);
+    m_realtimeToggleAction = new QAction(this);
+    connect(m_realtimeToggleAction, &QAction::triggered, this, [this]() {
+        if (m_status == Status::Deactivating) {
+            if (m_partialActivationFactor < 0.5) {
+                deactivate(false);
+            } else {
+                cancelPartialDeactivate();
+            }
+        } else if (m_status == Status::Activating) {
+            if (m_partialActivationFactor > 0.5) {
+                activate();
+            } else {
+                cancelPartialActivate();
+            }
+        }
+    });
+
+    auto progressCallback = [this](qreal progress) {
+        if (!effects->hasActiveFullScreenEffect() || effects->activeFullScreenEffect() == this) {
+            switch (m_status) {
+            case Status::Inactive:
+            case Status::Activating:
+                partialActivate(progress);
+                break;
+            case Status::Active:
+            case Status::Deactivating:
+                partialDeactivate(progress);
+                break;
+            }
+        }
+    };
+
+    effects->registerTouchpadPinchShortcut(PinchDirection::Contracting, 4, m_realtimeToggleAction, progressCallback);
+    effects->registerTouchscreenSwipeShortcut(SwipeDirection::Up, 3, m_realtimeToggleAction, progressCallback);
+
+    connect(effects, &EffectsHandler::screenAboutToLock, this, &MobileTaskSwitcherEffect::realDeactivate);
 
     setSource(QUrl::fromLocalFile(
         QStandardPaths::locate(QStandardPaths::GenericDataLocation, QStringLiteral("kwin/effects/mobiletaskswitcher/qml/TaskSwitcher.qml"))));
@@ -35,18 +73,54 @@ MobileTaskSwitcherEffect::~MobileTaskSwitcherEffect()
 {
 }
 
+void MobileTaskSwitcherEffect::reconfigure(ReconfigureFlags)
+{
+    setAnimationDuration(animationTime(300));
+
+    for (const ElectricBorder &border : std::as_const(m_borderActivate)) {
+        effects->unreserveElectricBorder(border, this);
+    }
+
+    for (const ElectricBorder &border : std::as_const(m_touchBorderActivate)) {
+        effects->unregisterTouchBorder(border, m_toggleAction);
+    }
+
+    m_borderActivate.clear();
+    m_touchBorderActivate.clear();
+
+    const QList<int> activateBorders = {ElectricBorder::ElectricBottom};
+    for (const int &border : activateBorders) {
+        m_borderActivate.append(ElectricBorder(border));
+        effects->reserveElectricBorder(ElectricBorder(border), this);
+    }
+
+    const QList<int> touchActivateBorders = {ElectricBorder::ElectricBottom};
+    for (const int &border : touchActivateBorders) {
+        m_touchBorderActivate.append(ElectricBorder(border));
+        effects->registerRealtimeTouchBorder(ElectricBorder(border),
+                                             m_realtimeToggleAction,
+                                             [this](ElectricBorder border, const QPointF &deltaProgress, const EffectScreen *screen) {
+                                                 if (m_status == Status::Active) {
+                                                     return;
+                                                 }
+                                                 const int maxDelta = 500; // Arbitrary logical pixels value seems to behave better than scaledScreenSize
+                                                 if (border == ElectricTop || border == ElectricBottom) {
+                                                     partialActivate(std::min(1.0, std::abs(deltaProgress.y()) / maxDelta));
+                                                 } else {
+                                                     partialActivate(std::min(1.0, std::abs(deltaProgress.x()) / maxDelta));
+                                                 }
+                                             });
+    }
+}
+
 int MobileTaskSwitcherEffect::requestedEffectChainPosition() const
 {
-    return 0;
+    return 70;
 }
 
 bool MobileTaskSwitcherEffect::borderActivated(ElectricBorder border)
 {
     return false;
-}
-
-void MobileTaskSwitcherEffect::reconfigure(ReconfigureFlags flags)
-{
 }
 
 void MobileTaskSwitcherEffect::grabbedKeyboardEvent(QKeyEvent *keyEvent)
@@ -60,18 +134,12 @@ void MobileTaskSwitcherEffect::grabbedKeyboardEvent(QKeyEvent *keyEvent)
     QuickSceneEffect::grabbedKeyboardEvent(keyEvent);
 }
 
-void MobileTaskSwitcherEffect::realDeactivate()
-{
-    setRunning(false);
-    m_status = Status::Inactive;
-}
-
 void MobileTaskSwitcherEffect::toggle()
 {
     if (!isRunning()) {
         activate();
     } else {
-        deactivate();
+        deactivate(false);
     }
 }
 
@@ -85,9 +153,99 @@ void MobileTaskSwitcherEffect::activate()
     setRunning(true);
 }
 
-void MobileTaskSwitcherEffect::deactivate()
+void MobileTaskSwitcherEffect::deactivate(bool deactivateInstantly)
+{
+    const auto screens = effects->screens();
+    for (const auto screen : screens) {
+        if (QuickSceneView *view = viewForScreen(screen)) {
+            QMetaObject::invokeMethod(view->rootItem(), "hideAnimation");
+        }
+    }
+    m_shutdownTimer->start(animationTime(deactivateInstantly ? 0 : 200));
+
+    setGestureInProgress(false);
+    setPartialActivationFactor(0.0);
+}
+
+void MobileTaskSwitcherEffect::partialActivate(qreal factor)
+{
+    if (effects->isScreenLocked()) {
+        return;
+    }
+
+    m_status = Status::Activating;
+
+    setPartialActivationFactor(factor);
+    setGestureInProgress(true);
+
+    setRunning(true);
+}
+
+void MobileTaskSwitcherEffect::partialDeactivate(qreal factor)
+{
+    m_status = Status::Deactivating;
+
+    setPartialActivationFactor(1.0 - factor);
+    setGestureInProgress(true);
+}
+
+void MobileTaskSwitcherEffect::cancelPartialDeactivate()
+{
+    activate();
+}
+
+void MobileTaskSwitcherEffect::cancelPartialActivate()
+{
+    deactivate(false);
+}
+
+void MobileTaskSwitcherEffect::realDeactivate()
 {
     setRunning(false);
     m_status = Status::Inactive;
+}
+
+void MobileTaskSwitcherEffect::quickDeactivate()
+{
+    m_shutdownTimer->start(0);
+}
+
+int MobileTaskSwitcherEffect::animationDuration() const
+{
+    return m_animationDuration;
+}
+
+void MobileTaskSwitcherEffect::setAnimationDuration(int duration)
+{
+    if (m_animationDuration != duration) {
+        m_animationDuration = duration;
+        Q_EMIT animationDurationChanged();
+    }
+}
+
+bool MobileTaskSwitcherEffect::gestureInProgress() const
+{
+    return m_gestureInProgress;
+}
+
+void MobileTaskSwitcherEffect::setGestureInProgress(bool gesture)
+{
+    if (m_gestureInProgress != gesture) {
+        m_gestureInProgress = gesture;
+        Q_EMIT gestureInProgressChanged();
+    }
+}
+
+qreal MobileTaskSwitcherEffect::partialActivationFactor() const
+{
+    return m_partialActivationFactor;
+}
+
+void MobileTaskSwitcherEffect::setPartialActivationFactor(qreal factor)
+{
+    if (m_partialActivationFactor != factor) {
+        m_partialActivationFactor = factor;
+        Q_EMIT partialActivationFactorChanged();
+    }
 }
 }
--- kwin/mobiletaskswitcher/mobiletaskswitchereffect.h
+++ kwin/mobiletaskswitcher/mobiletaskswitchereffect.h
@@ -1,4 +1,5 @@
-// SPDX-FileCopyrightText: 2022 Devin Lin <devin@kde.org>
+// SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+// SPDX-FileCopyrightText: 2023 Devin Lin <devin@kde.org>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
@@ -9,14 +10,10 @@
 
 #include <QAction>
 #include <QKeySequence>
+#include <QTimer>
 
 #include <KGlobalAccel>
 #include <KLocalizedString>
-
-/**
- * Feature Scope:
- * -
- */
 
 namespace KWin
 {
@@ -30,21 +27,49 @@ public:
     MobileTaskSwitcherEffect();
     ~MobileTaskSwitcherEffect() override;
 
+    int animationDuration() const;
+    void setAnimationDuration(int duration);
+
+    bool gestureInProgress() const;
+    void setGestureInProgress(bool gesture);
+
+    qreal partialActivationFactor() const;
+    void setPartialActivationFactor(qreal factor);
+
     int requestedEffectChainPosition() const override;
     bool borderActivated(ElectricBorder border) override;
     void reconfigure(ReconfigureFlags flags) override;
     void grabbedKeyboardEvent(QKeyEvent *keyEvent) override;
 
 public Q_SLOTS:
-    void realDeactivate();
-    void toggle();
     void activate();
-    void deactivate();
+    void realDeactivate();
+    void partialActivate(qreal factor);
+    void cancelPartialActivate();
+    void partialDeactivate(qreal factor);
+    void cancelPartialDeactivate();
+    void deactivate(bool deactivateInstantly);
+    void quickDeactivate();
+    void toggle();
+
+Q_SIGNALS:
+    void animationDurationChanged();
+    void gestureInProgressChanged();
+    void partialActivationFactorChanged();
 
 private:
+    QAction *m_realtimeToggleAction = nullptr;
     QAction *m_toggleAction = nullptr;
     QList<QKeySequence> m_toggleShortcut;
     Status m_status = Status::Inactive;
+    ;
+    QTimer *m_shutdownTimer;
+    QList<ElectricBorder> m_borderActivate;
+    QList<ElectricBorder> m_touchBorderActivate;
+
+    int m_animationDuration = 400;
+    qreal m_partialActivationFactor = 0;
+    bool m_gestureInProgress = false;
 };
 
 } // namespace KWin
