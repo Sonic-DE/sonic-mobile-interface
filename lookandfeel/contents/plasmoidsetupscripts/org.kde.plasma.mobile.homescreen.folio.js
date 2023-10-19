fc211b54009329b16a26f2c80f017ecf514f6af7
Devin Lin <espidev@gmail.com>
2023-10-18 23:44:51 -0700

    remove init script

--- lookandfeel/contents/plasmoidsetupscripts/org.kde.plasma.mobile.homescreen.folio.js
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2019-2020 Marco Martin <mart@kde.org>
-// SPDX-FileCopyrightText: 2021 Devin Lin <devin@kde.org>
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-applet.wallpaperPlugin = 'org.kde.image'
-applet.writeConfig("Favourites", `[{"storageId": "org.kde.phone.dialer.desktop", "type": "application"}, {"storageId": "org.kde.spacebar.desktop", "type": "application"},{"storageId": "org.kde.angelfish.desktop", "type": "application"}]`);
-applet.writeConfig("Pages", `[[{"column": 0, "row": 0, "storageId": "org.kde.qmlkonsole.desktop", "type": "application"}, {"column": 1, "row": 0, "storageId": "org.kde.kclock.desktop", "type": "application"}, {"column": 2, "row": 0, "storageId": "org.kde.kweather.desktop", "type": "application"}, {"column": 3, "row": 0, "storageId": "org.kde.krecorder.desktop", "type": "application"}]]`);
-applet.reloadConfig()
-
