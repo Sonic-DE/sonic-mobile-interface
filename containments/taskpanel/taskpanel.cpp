96bcaade79d45d34d5a8b9c757df14ea2387f695
Devin Lin <espidev@gmail.com>
2024-11-07 23:52:45 -0800

    navigationpanel: Add manual screen rotation button
    When the device is rotated and autorotate is off, a button will appear
    on the navigation panel to manually rotate.
    
    This is specific to the navigation panel, perhaps we can have a floating
    button in gesture-only mode similar to Android?
    
    TODO: PoC, I haven't yet actually tested this on device. I also need to
    make sure that I check for whether autorotation is enabled on the
    output.

--- containments/taskpanel/taskpanel.cpp
+++ containments/taskpanel/taskpanel.cpp
@@ -11,12 +11,43 @@
 #include <QDebug>
 #include <QGuiApplication>
 
+#include <kscreen/configmonitor.h>
+#include <kscreen/getconfigoperation.h>
+#include <kscreen/output.h>
+#include <kscreen/setconfigoperation.h>
+
 // register type for Keyboards.KWinVirtualKeyboard.forceActivate();
 Q_DECLARE_METATYPE(QDBusPendingReply<>)
 
+KScreen::Output::Rotation mapReadingOrientation(QOrientationReading::Orientation orientation)
+{
+    switch (orientation) {
+    case QOrientationReading::Orientation::TopUp:
+        return KScreen::Output::Rotation::None;
+    case QOrientationReading::Orientation::TopDown:
+        return KScreen::Output::Rotation::Inverted;
+    case QOrientationReading::Orientation::LeftUp:
+        return KScreen::Output::Rotation::Right;
+    case QOrientationReading::Orientation::RightUp:
+        return KScreen::Output::Rotation::Left;
+    case QOrientationReading::Orientation::FaceUp:
+    case QOrientationReading::Orientation::FaceDown:
+    case QOrientationReading::Orientation::Undefined:
+        return KScreen::Output::Rotation::None;
+    }
+    return KScreen::Output::Rotation::None;
+}
+
 TaskPanel::TaskPanel(QObject *parent, const KPluginMetaData &data, const QVariantList &args)
     : Plasma::Containment(parent, data, args)
+    , m_sensor{new QOrientationSensor(this)}
 {
+    connect(new KScreen::GetConfigOperation(), &KScreen::GetConfigOperation::finished, this, [this](auto *op) {
+        m_config = qobject_cast<KScreen::GetConfigOperation *>(op)->config();
+        KScreen::ConfigMonitor::instance()->addConfig(m_config);
+    });
+
+    connect(m_sensor, &QOrientationSensor::readingChanged, this, &TaskPanel::updateShowRotationButton);
 }
 
 void TaskPanel::triggerTaskSwitcher() const
@@ -26,6 +57,45 @@ void TaskPanel::triggerTaskSwitcher() const
 
     // this does not block, so it won't necessarily be called before the method returns
     QDBusConnection::sessionBus().send(message);
+}
+
+void TaskPanel::rotateToSuggestedRotation()
+{
+    const auto outputs = m_config->outputs();
+    if (outputs.empty()) {
+        return;
+    }
+
+    // HACK: Assume the output we care about is the first device
+    const auto output = outputs[0];
+    output->setRotation(m_rotateTo);
+
+    auto setop = new KScreen::SetConfigOperation(m_config, this);
+    setop->exec();
+}
+
+bool TaskPanel::showRotationButton() const
+{
+    return m_showRotationButton;
+}
+
+void TaskPanel::updateShowRotationButton()
+{
+    QOrientationReading *reading = m_sensor->reading();
+    m_rotateTo = mapReadingOrientation(reading->orientation());
+
+    const auto outputs = m_config->outputs();
+
+    if (outputs.empty()) {
+        m_showRotationButton = false;
+        Q_EMIT showRotationButtonChanged();
+        return;
+    }
+
+    // HACK: Assume the output we care about is the first device
+    const auto output = outputs[0];
+    m_showRotationButton = output->rotation() != m_rotateTo;
+    Q_EMIT showRotationButtonChanged();
 }
 
 K_PLUGIN_CLASS(TaskPanel)
