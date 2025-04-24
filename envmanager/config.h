6401b4d4c0b90232f7b8404eb864190ee25ceaaa
Devin Lin <espidev@gmail.com>
2025-04-24 17:26:11 +0200

    envmanager: Use KWin overlay config
    We currently directly write our settings to ~/.config/kwinrc. Instead, create a separate config file with our settings, and use a feature in KWin to overlay that config over kwinrc.

--- envmanager/config.h
+++ envmanager/config.h
@@ -32,7 +32,7 @@ const QMap<QString, QMap<QString, QVariant>> KDEGLOBALS_DEFAULT_SETTINGS = {{"Ge
 
 const QMap<QString, QMap<QString, QVariant>> KDEGLOBALS_SETTINGS = {{"KDE", {{"LookAndFeelPackage", "org.kde.breeze.mobile"}}}};
 
-// kwinrc
+// kwinrc-plasma-mobile
 QMap<QString, QMap<QString, QVariant>> getKwinrcSettings(KSharedConfig::Ptr m_mobileConfig)
 {
     auto group = KConfigGroup{m_mobileConfig, QStringLiteral("General")};
