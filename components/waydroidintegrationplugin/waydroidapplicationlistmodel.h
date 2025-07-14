f85d4b1578fced9a8e6069d4687e8277835dba8b
Florian RICHER <florian.richer@protonmail.com>
2025-07-14 12:47:21 +0200

    Implement Waydroid Applications

---
+++ components/waydroidintegrationplugin/waydroidapplicationlistmodel.h
@@ -0,0 +1,25 @@
+/*
+ *   SPDX-FileCopyrightText: 2025 Florian RICHER <florian.richer@protonmail.com>
+ *
+ *   SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+#include "waydroidapplication.h"
+
+#include <QAbstractListModel>
+#include <QObject>
+
+class WaydroidApplicationListModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    WaydroidApplicationListModel(QObject *parent = nullptr);
+
+private:
+    QList<WaydroidApplication::Ptr> m_applications;
+
+    void load();
+};
