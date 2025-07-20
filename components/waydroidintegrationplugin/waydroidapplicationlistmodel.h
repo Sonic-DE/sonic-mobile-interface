KDE/plasma-mobile

kcm: Implement Waydroid Applications

components/waydroidintegrationplugin/waydroidapplicationlistmodel.h
@@ -1,0 +1,50 @@
+/*
+ *   SPDX-FileCopyrightText: 2025 Florian RICHER <florian.richer@protonmail.com>
+ *
+ *   SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+#include "waydroidapplication.h"
+#include "waydroidstate.h"
+
+#include <QAbstractListModel>
+#include <QObject>
+#include <QTimer>
+
+class WaydroidState;
+
+class WaydroidApplicationListModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    enum Roles {
+        DelegateRole = Qt::UserRole + 1,
+        NameRole,
+        IdRole
+    };
+
+    WaydroidApplicationListModel(WaydroidState *parent = nullptr);
+    ~WaydroidApplicationListModel() override;
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+    QHash<int, QByteArray> roleNames() const Q_DECL_OVERRIDE;
+
+    Q_INVOKABLE void installApk(const QString apkFile);
+    Q_INVOKABLE void deleteApplication(const QString appId);
+
+Q_SIGNALS:
+    void actionFinished(const QString message);
+    void errorOccurred(const QString message);
+
+private:
+    WaydroidState *m_waydroidState{nullptr};
+    QList<WaydroidApplication::Ptr> m_applications;
+    QTimer *m_refreshTimer{nullptr};
+
+    void refreshApplications();
+    QList<WaydroidApplication::Ptr> queryApplications() const;
+};

kcms/waydroidintegration/ui/WaydroidConfigurationForm.qml
@@ -42,10 +42,14 @@ ColumnLayout {
         }
 
         FormCard.FormButtonDelegate {
-            id: quickSettingsButton
             visible: AIP.WaydroidState.systemType === AIP.WaydroidState.Gapps
             text: i18n("Certify my device for Google Play Protect")
             onClicked: kcm.push("WaydroidGooglePlayProtectConfigurationPage.qml")
+        }
+
+        FormCard.FormButtonDelegate {
+            text: i18n("Waydroid applications")
+            onClicked: kcm.push("WaydroidApplicationsPage.qml")
         }
     }
 

Sonic-DE/sonic-mobile-interface

components/waydroidintegrationplugin/waydroidapplicationlistmodel.h
@@ -1,0 +1,50 @@
+/*
+ *   SPDX-FileCopyrightText: 2025 Florian RICHER <florian.richer@protonmail.com>
+ *
+ *   SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+#include "waydroidapplication.h"
+#include "waydroidstate.h"
+
+#include <QAbstractListModel>
+#include <QObject>
+#include <QTimer>
+
+class WaydroidState;
+
+class WaydroidApplicationListModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    enum Roles {
+        DelegateRole = Qt::UserRole + 1,
+        NameRole,
+        IdRole
+    };
+
+    WaydroidApplicationListModel(WaydroidState *parent = nullptr);
+    ~WaydroidApplicationListModel() override;
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    Q_INVOKABLE void installApk(const QString apkFile);
+    Q_INVOKABLE void deleteApplication(const QString appId);
+
+Q_SIGNALS:
+    void actionFinished(const QString message);
+    void errorOccurred(const QString message);
+
+private:
+    WaydroidState *m_waydroidState{nullptr};
+    QList<WaydroidApplication::Ptr> m_applications;
+    QTimer *m_refreshTimer{nullptr};
+
+    void refreshApplications();
+    QList<WaydroidApplication::Ptr> queryApplications() const;
+};

kcms/waydroidintegration/ui/WaydroidConfigurationForm.qml
@@ -42,10 +42,14 @@ ColumnLayout {
         }
 
         FormCard.FormButtonDelegate {
-            id: quickSettingsButton
             visible: AIP.WaydroidState.systemType === AIP.WaydroidState.Gapps
             text: i18n("Certify my device for Google Play Protect")
             onClicked: kcm.push("WaydroidGooglePlayProtectConfigurationPage.qml")
+        }
+
+        FormCard.FormButtonDelegate {
+            text: i18n("Installed applications")
+            onClicked: kcm.push("WaydroidApplicationsPage.qml")
         }
     }
 