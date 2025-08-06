d3c7b8ecd1bdf9889e763bf38deaf57b00714128
Florian RICHER <florian.richer@protonmail.com>
2025-08-06 21:48:27 +0000

    waydroid: Begin implement DBus

--- components/waydroidintegrationplugin/waydroiddbusobject.cpp
+++ components/waydroidintegrationplugin/waydroiddbusobject.cpp
@@ -6,11 +6,29 @@
 
 #include "waydroiddbusobject.h"
 #include "waydroidintegrationplugin_debug.h"
+#include "waydroidshared.h"
 
 #include <QDBusConnection>
+#include <QDir>
 #include <QLoggingCategory>
+#include <QProcess>
+#include <QRegularExpression>
+#include <QStandardPaths>
+#include <QTimer>
+
+#include <KConfigGroup>
+#include <KDesktopFile>
+#include <KSandbox>
 
 using namespace Qt::StringLiterals;
+
+#define MULTI_WINDOWS_PROP_KEY "persist.waydroid.multi_windows"
+#define SUSPEND_PROP_KEY "persist.waydroid.suspend"
+#define UEVENT_PROP_KEY "persist.waydroid.uevent"
+
+static const QRegularExpression sessionRegExp(u"Session:\\s*(\\w+)"_s);
+static const QRegularExpression ipAdressRegExp(u"IP address:\\s*(\\d+\\.\\d+\\.\\d+\\.\\d+)"_s);
+static const QRegularExpression systemOtaRegExp(u"system_ota\\s*=\\s*(\\S+)"_s);
 
 WaydroidDBusObject::WaydroidDBusObject(QObject *parent)
     : QObject{parent}
@@ -19,9 +37,196 @@ WaydroidDBusObject::WaydroidDBusObject(QObject *parent)
 
 void WaydroidDBusObject::registerObject()
 {
-    if (!m_initialized) {
+    if (!m_dbusInitialized) {
         QDBusConnection::sessionBus().registerObject(u"/Waydroid"_s, this);
-        m_initialized = true;
-        qCWarning(WAYDROIDINTEGRATIONPLUGIN) << "Initialized";
+        m_dbusInitialized = true;
+
+        // Connect it-self to auto-refresh when required status has changed
+        connect(this, &WaydroidDBusObject::statusChanged, this, &WaydroidDBusObject::refreshSessionInfo);
+
+        refreshSupportsInfo();
     }
+}
+
+WaydroidDBusObject::Status WaydroidDBusObject::status() const
+{
+    return m_status;
+}
+
+WaydroidDBusObject::SessionStatus WaydroidDBusObject::sessionStatus() const
+{
+    return m_sessionStatus;
+}
+
+void WaydroidDBusObject::refreshSupportsInfo()
+{
+    const QStringList arguments{u"-h"_s};
+
+    auto process = QProcess(this);
+    process.start(WAYDROID_COMMAND, arguments);
+    process.waitForFinished();
+
+    const int exitCode = process.exitCode();
+    if (exitCode != 0) {
+        m_status = NotSupported;
+        Q_EMIT statusChanged();
+        return;
+    }
+
+    const QString output = fetchSessionInfo();
+    if (!output.contains("WayDroid is not initialized")) {
+        m_status = Initialized;
+    } else {
+        m_status = NotInitialized;
+    }
+    Q_EMIT statusChanged();
+}
+
+void WaydroidDBusObject::refreshSessionInfo()
+{
+    if (m_status != Initialized) {
+        return;
+    }
+
+    const QString output = fetchSessionInfo();
+
+    const QString sessionMatchResult = extractRegExp(output, sessionRegExp);
+    SessionStatus newSessionStatus;
+
+    if (!sessionMatchResult.isEmpty()) {
+        newSessionStatus = sessionMatchResult.contains("RUNNING") ? SessionRunning : SessionStopped;
+    } else {
+        newSessionStatus = SessionStopped;
+    }
+
+    if (m_sessionStatus != newSessionStatus) {
+        m_sessionStatus = newSessionStatus;
+        Q_EMIT sessionStatusChanged();
+    }
+
+    m_ipAddress = extractRegExp(output, ipAdressRegExp);
+    Q_EMIT ipAddressChanged();
+}
+
+QString WaydroidDBusObject::fetchSessionInfo()
+{
+    const QStringList arguments{u"status"_s};
+
+    auto process = QProcess(this);
+    process.start(WAYDROID_COMMAND, arguments);
+    process.waitForFinished();
+
+    return process.readAllStandardOutput();
+}
+
+QString WaydroidDBusObject::fetchPropValue(const QString key, const QString defaultValue)
+{
+    const QStringList arguments{u"prop"_s, u"get"_s, key};
+
+    QProcess *process = new QProcess(this);
+    process->start(WAYDROID_COMMAND, arguments);
+    process->waitForFinished();
+
+    const QString commandOutput = process->readAllStandardOutput();
+    const QString value = commandOutput.split("\n").first().trimmed();
+
+    if (value.isEmpty()) {
+        return defaultValue;
+    }
+
+    return value;
+}
+
+bool WaydroidDBusObject::writePropValue(const QString key, const QString value)
+{
+    const QStringList arguments{u"prop"_s, u"set"_s, key, value};
+
+    auto process = QProcess(this);
+    process.start(WAYDROID_COMMAND, arguments);
+    process.waitForFinished();
+
+    return process.exitCode() == 0;
+}
+
+QString WaydroidDBusObject::extractRegExp(const QString text, const QRegularExpression regExp) const
+{
+    const QRegularExpressionMatch match = regExp.match(text);
+
+    if (match.hasMatch() && match.lastCapturedIndex() > 0) {
+        return match.captured(match.lastCapturedIndex());
+    } else {
+        return "";
+    }
+}
+
+void WaydroidDBusObject::checkSessionStarting(const int limit, const int tried)
+{
+    if (m_sessionStatus != SessionStarting) {
+        return;
+    }
+
+    const QString output = fetchSessionInfo();
+    const QString sessionMatchResult = extractRegExp(output, sessionRegExp);
+
+    if (sessionMatchResult.contains("RUNNING")) {
+        m_sessionStatus = SessionRunning;
+        Q_EMIT sessionStatusChanged();
+    } else if (tried == limit) {
+        m_sessionStatus = SessionStopped;
+        Q_EMIT sessionStatusChanged();
+        qCWarning(WAYDROIDINTEGRATIONPLUGIN) << "Failed to start the session after " << tried << " tries";
+    } else {
+        QTimer::singleShot(500, [this, tried, limit]() {
+            checkSessionStarting(limit, tried + 1);
+        });
+    }
+}
+
+QString WaydroidDBusObject::desktopFileDirectory()
+{
+    auto dir = []() -> QString {
+        if (KSandbox::isFlatpak()) {
+            return qEnvironmentVariable("HOME") % u"/.local/share/applications/";
+        }
+        return QStandardPaths::writableLocation(QStandardPaths::ApplicationsLocation);
+    }();
+
+    QDir(dir).mkpath(QStringLiteral("."));
+
+    return dir;
+}
+
+bool WaydroidDBusObject::removeWaydroidApplications()
+{
+    const QDir appsDir(desktopFileDirectory());
+    const auto fileInfos = appsDir.entryInfoList(QDir::Files);
+    if (fileInfos.length() < 1) {
+        return false;
+    }
+
+    bool allFileRemoved = true;
+
+    for (const auto &fileInfo : fileInfos) {
+        if (fileInfo.fileName().contains(QStringView(u".desktop"))) {
+            const KDesktopFile desktopFile(fileInfo.filePath());
+            const KConfigGroup configGroup = desktopFile.desktopGroup();
+
+            if (!configGroup.hasKey(u"Categories"_s)) {
+                continue;
+            }
+
+            const auto categories = configGroup.readEntry(u"Categories"_s);
+            if (!categories.contains(u"X-WayDroid-App"_s)) {
+                continue;
+            }
+
+            QFile file(fileInfo.filePath());
+            if (!file.remove()) {
+                allFileRemoved &= false;
+                qCWarning(WAYDROIDINTEGRATIONPLUGIN) << "Failed to remove: " << desktopFile.name();
+            }
+        }
+    }
+
+    return allFileRemoved;
 }
--- components/waydroidintegrationplugin/waydroiddbusobject.h
+++ components/waydroidintegrationplugin/waydroiddbusobject.h
@@ -11,6 +11,12 @@
 
 #include <qqmlregistration.h>
 
+/**
+ * This class provides an interface to interact with the Waydroid container,
+ * including session management and property configuration.
+ *
+ * @author Florian RICHER <florian.richer@protonmail.com>
+ */
 class WaydroidDBusObject : public QObject
 {
     Q_OBJECT
@@ -21,9 +27,127 @@ class WaydroidDBusObject : public QObject
 public:
     WaydroidDBusObject(QObject *parent = nullptr);
 
+    /**
+     * @enum Status
+     * @brief Defines the possible installation statuses of the Waydroid service.
+     */
+    enum Status {
+        NotSupported = 0,
+        NotInitialized,
+        Initializing,
+        Initialized,
+        Resetting
+    };
+    Q_ENUM(Status)
+
+    /**
+     * @enum SessionStatus
+     * @brief Defines the possible states of a Waydroid session.
+     */
+    enum SessionStatus {
+        SessionStopped = 0,
+        SessionStarting,
+        SessionRunning
+    };
+    Q_ENUM(SessionStatus)
+
+    /**
+     * @enum SystemType
+     * @brief Defines the types of Android systems supported by Waydroid.
+     */
+    enum SystemType {
+        Vanilla = 0, ///< Vanilla Android system.
+        Foss, ///< Free and Open Source Software variant.
+        Gapps, ///< Variant with Google Apps included.
+        UnknownSystemType
+    };
+    Q_ENUM(SystemType)
+
+    /**
+     * @enum RomType
+     * @brief Defines the types of ROMs supported by Waydroid.
+     *
+     * @todo Add OTA ROM with custom system url and vendor url
+     */
+    enum RomType {
+        Lineage = 0, ///< LineageOS ROM.
+        Bliss ///< Bliss ROM.
+    };
+    Q_ENUM(RomType)
+
     // called by QML
     Q_INVOKABLE void registerObject();
 
+Q_SIGNALS:
+    Q_SCRIPTABLE void statusChanged();
+    Q_SCRIPTABLE void sessionStatusChanged();
+    Q_SCRIPTABLE void ipAddressChanged();
+
+public Q_SLOTS:
+    Q_SCRIPTABLE Status status() const;
+    Q_SCRIPTABLE SessionStatus sessionStatus() const;
+
 private:
-    bool m_initialized{false};
+    bool m_dbusInitialized{false};
+    Status m_status{NotInitialized};
+    SessionStatus m_sessionStatus{SessionStopped};
+    QString m_ipAddress{""};
+
+    void refreshSupportsInfo();
+    void refreshSessionInfo();
+
+    /**
+     * @brief Executes the command to retrieve the current session status and related
+     * information from Waydroid.
+     *
+     * @return A QString containing the output of the Waydroid session status command.
+     */
+    QString fetchSessionInfo();
+
+    /**
+     * @brief Executes the command to retrieve the value of a specified property from the Waydroid container.
+     *
+     * @param key The key of the property to fetch.
+     * @param defaultValue The default value to return if the property is not found or empty.
+     * @return A QString containing the property value, or the defaultValue if not found.
+     */
+    QString fetchPropValue(const QString key, const QString defaultValue);
+
+    /**
+     * @brief Executes the command to writes a value to a specified property in the Waydroid container.
+     *
+     * @param key The key of the property to set.
+     * @param value The value to write to the property.
+     * @return A boolean indicating whether the write operation was successful.
+     */
+    bool writePropValue(const QString key, const QString value);
+
+    /**
+     * @brief Extracts text from a string using a regular expression pattern.
+     *
+     * @param text The text to search within.
+     * @param regExp The regular expression pattern to use for extraction.
+     * @return A QString containing the extracted text if a match is found; otherwise, an empty string.
+     */
+    QString extractRegExp(const QString text, const QRegularExpression regExp) const;
+
+    /**
+     * @brief Checks every 500ms if the session has started.
+     *
+     * This function periodically checks whether a session has started. If the session starts,
+     * it emits a "Running" signal. If the check count reaches the specified limit without
+     * the session starting, it emits a "Stopped" signal and logs a warning message.
+     *
+     * @param limit The maximum number of attempts to check for session start before stopping.
+     * @param tried The current number of attempts made to check for session start (defaults to 0).
+     *
+     * @todo Investigate using DBus for a cleaner implementation, potentially using the method:
+     *       id.waydro.Container /ContainerManager id.waydro.ContainerManager.Start(a{ss} session).
+     *       This would require duplicating the session start command logic from:
+     *       https://github.com/waydroid/waydroid/blob/2c41162d8bfef5bf83333a6ce4834af0c3c2b535/tools/actions/session_manager.py#L31
+     */
+    void checkSessionStarting(const int limit, const int tried = 0);
+
+    QString desktopFileDirectory();
+    bool removeWaydroidApplications();
 };
