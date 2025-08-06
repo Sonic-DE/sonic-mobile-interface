71e80c1318ca2675c16f05961a4005b1282fc993
Florian RICHER <florian.richer@protonmail.com>
2025-08-06 20:06:17 +0000

    waydroid: Migrate to DBus backend implementations

---
+++ components/waydroidintegrationplugin/waydroidapplicationdbusclient.cpp
@@ -0,0 +1,93 @@
+/*
+ *   SPDX-FileCopyrightText: 2025 Florian RICHER <florian.richer@protonmail.com>
+ *
+ *   SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "waydroidapplicationdbusclient.h"
+
+#include <QDBusConnection>
+
+using namespace Qt::StringLiterals;
+
+WaydroidApplicationDBusClient::WaydroidApplicationDBusClient(const QDBusObjectPath &objectPath, QObject *parent)
+    : QObject{parent}
+    , m_objectPath{objectPath}
+    , m_interface{new OrgKdePlasmashellWaydroidApplicationInterface{u"org.kde.plasmashell"_s, objectPath.path(), QDBusConnection::sessionBus(), this}}
+{
+    // Check if the service is already running
+    if (QDBusConnection::sessionBus().interface()->isServiceRegistered(u"org.kde.plasmashell"_s)) {
+        m_connected = true;
+        if (m_interface->isValid()) {
+            // Initialize properties
+            updateName();
+            updatePackageName();
+        }
+    }
+
+    connect(QDBusConnection::sessionBus().interface(),
+            &QDBusConnectionInterface::serviceOwnerChanged,
+            this,
+            [this](const QString &service, const QString &oldOwner, const QString &newOwner) {
+                if (service == u"org.kde.plasmashell"_s) {
+                    if (newOwner.isEmpty()) {
+                        // Service stopped
+                        m_connected = false;
+                    } else if (oldOwner.isEmpty()) {
+                        // Service started
+                        m_connected = true;
+                        if (m_interface->isValid()) {
+                            updateName();
+                            updatePackageName();
+                        }
+                    }
+                }
+            });
+}
+
+QString WaydroidApplicationDBusClient::name() const
+{
+    return m_name;
+}
+
+QString WaydroidApplicationDBusClient::packageName() const
+{
+    return m_packageName;
+}
+
+QDBusObjectPath WaydroidApplicationDBusClient::objectPath() const
+{
+    return m_objectPath;
+}
+
+void WaydroidApplicationDBusClient::updateName()
+{
+    auto reply = m_interface->name();
+    auto watcher = new QDBusPendingCallWatcher(reply, this);
+
+    connect(watcher, &QDBusPendingCallWatcher::finished, this, [this](auto watcher) {
+        QDBusPendingReply<QString> reply = *watcher;
+        const auto name = reply.argumentAt<0>();
+
+        if (m_name != name) {
+            m_name = name;
+            Q_EMIT nameChanged();
+        }
+    });
+}
+
+void WaydroidApplicationDBusClient::updatePackageName()
+{
+    auto reply = m_interface->name();
+    auto watcher = new QDBusPendingCallWatcher(reply, this);
+
+    connect(watcher, &QDBusPendingCallWatcher::finished, this, [this](auto watcher) {
+        QDBusPendingReply<QString> reply = *watcher;
+        const auto packageName = reply.argumentAt<0>();
+
+        if (m_packageName != packageName) {
+            m_packageName = packageName;
+            Q_EMIT packageNameChanged();
+        }
+    });
+}
---
+++ components/waydroidintegrationplugin/waydroidapplicationdbusclient.h
@@ -0,0 +1,53 @@
+/*
+ *   SPDX-FileCopyrightText: 2025 Florian RICHER <florian.richer@protonmail.com>
+ *
+ *   SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+#include "plasmashellwaydroidapplicationinterface.h"
+
+#include <QObject>
+#include <QString>
+
+#include <qqmlregistration.h>
+
+/**
+ * This class provides a DBus client interface for individual Waydroid application.
+ * It connects to WaydroidApplicationDBusObject instances via DBus.
+ *
+ * @author Florian RICHER <florian.richer@protonmail.com>
+ */
+class WaydroidApplicationDBusClient : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+
+    Q_PROPERTY(QString name READ name CONSTANT)
+    Q_PROPERTY(QString packageName READ packageName CONSTANT)
+
+public:
+    typedef std::shared_ptr<WaydroidApplicationDBusClient> Ptr;
+
+    explicit WaydroidApplicationDBusClient(const QDBusObjectPath &objectPath, QObject *parent = nullptr);
+
+    QString name() const;
+    QString packageName() const;
+    QDBusObjectPath objectPath() const;
+
+private Q_SLOTS:
+    void updateName();
+    void updatePackageName();
+
+Q_SIGNALS:
+    void nameChanged();
+    void packageNameChanged();
+
+private:
+    QString m_packageName;
+    QString m_name;
+    QDBusObjectPath m_objectPath;
+    OrgKdePlasmashellWaydroidApplicationInterface *m_interface;
+    bool m_connected{false};
+};
