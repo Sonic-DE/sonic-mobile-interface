e61efe5dc11165062ac0fe21c2d8aa0c91758503
Sebastian Kügler <sebas@kde.org>
2025-09-04 17:47:39 +0200

    new window management scripts
    This changeset reworks Plasma Mobile's window management.
    
    Instead of relying on kwinrc setting (and changing) policies for all
    windows, we now use a (new) javascript callback into kwin to allow for
    more fine-grained window management. This for example, allows us to
    avoid showing modal dialogs fullscreen for certain apps.
    
    This changeset implements more fine-grained window management using a
    javascript kwin script and the placement callback in this MR:
    https://invent.kde.org/plasma/kwin/-/merge_requests/8079
    
    Eventually, this MR supersedes the convergentwindows script and replaces
    it with two separate JavaScripts:
    - mobilewindows: used when non-docked
    - dockedwindows: used when docked
    
    The scripts loop over open windows when loaded and change to border
    shown and maximize when mobile, at least for non-modal windows.
    
    There's a change to plasma-mobile-envmanager here which makes the
    plasmashell load and unload the new mobilewindows and dockedwindows
    winscript when switching between "mobile" and convergenceMode.
    It also removes the Window section (may need some code for migration!).
    
    Signed-off-by: Sebastian Kügler <sebas@kde.org>

---
+++ kwin/scripts/dockedwindows/contents/code/main.js
@@ -0,0 +1,25 @@
+
+/**
+ * This script is run when Plasma Mobile switches to docked mode.
+ * It does two things:
+ *  - resets the placementCallback to null, so we have KWin decide
+ *    what to do with new windows
+ *  - switches on border for existing, normal windows
+ *
+ * It basically undoes what the mobilewindows script did.
+ *
+ */
+
+function init() {
+    const windows = workspace.stackingOrder;
+    for (var i = 0; i < windows.length; i++) {
+        var window = windows[i];
+        if (window.normalWindow) {
+            window.noBorder = false;
+        }
+    }
+}
+
+workspace.setPlacementCallback(null);
+
+init();
