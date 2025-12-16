d5e8b94bba57f4d3692ebed06a0184c9ebe8c2e6
Sebastian Kügler <sebas@kde.org>
2025-12-16 16:50:46 +0100

    fixes to script

--- kwin/scripts/mobilewindows/contents/code/main.js
+++ kwin/scripts/mobilewindows/contents/code/main.js
@@ -4,6 +4,10 @@ function dbg(msg) {
 }
 
 function analyze_window(w) {
+    if (!w) {
+        dbg("Window undefined or somesuch!");
+        return;
+    }
     dbg("___ Caption: " + w.caption + "______________");
     dbg("       windowRole: " + w.windowRole);
     dbg("     normalWindow: " + w.normalWindow);
@@ -12,6 +16,50 @@ function analyze_window(w) {
     dbg("    specialWindow: " + w.specialWindow);
     dbg("            modal: " + w.modal);
     dbg("____________________________________________");
+}
+
+
+function place(options) {
+    dbg("Hey there. installed script with NEW PARAMETERS is running!");
+    analyze_window(options.window);
+
+    var empty_rect = {};
+
+    let clientArea = options.area;
+    let window = options.window;
+
+
+    if (!window) {
+        dbg("Window pointer empty")
+        return empty_rect;
+    }
+
+    if (!window || window.resourceClass === 'xwaylandvideobridge') {
+        dbg("xwaylandvideobridge -> returning empty rect");
+        return empty_rect;
+    }
+
+    if (!window.modal) {
+        dbg("non modal, maximizing.");
+
+        var w_ = clientArea.width;
+        var h_ = clientArea.height;
+
+        //window.frameGeometry = {x: 0, y: 0, width: w_, height: h_ }
+        window.setMaximize(true, true);
+        window.noBorder = true;
+
+
+        var w_width = clientArea.width / 3;
+        var w_height = clientArea.height / 3;
+        return {x: 0, y: 0, width: w_width, height: w_height }; // FIXME: x and y.
+    } else {
+        dbg("modal, trying to ignore, returning empty rect")
+        return empty_rect;
+    }
+
+    dbg("fallthrough returning empty rect");
+    return empty_rect;
 }
 
 function fake_place(sss) {
@@ -25,7 +73,10 @@ function fake_place(sss) {
 }
 
 function init() {
-    const windows = workspace.windowList();
+    const windows = workspace.stackingOrder;
+
+    dbg("stackingOrder.length: " + workspace.stackingOrder.length);
+    dbg("windowList().length: " + workspace.windowList().length);
 
     dbg("I found " + windows.length + " windows. :)");
     for (var i = 0; i < windows.length; i++) {
@@ -34,19 +85,21 @@ function init() {
 
         const output = window.output;
         const desktop = window.desktops[0]; // assume it's the first desktop that the window is on
+        window.noBorder = true;
         analyze_window(window);
         if (desktop === undefined) {
             dbg("desktop undefined.");
             return;
         }
         const maximizeRect = workspace.clientArea(workspace.MaximizeArea, output, desktop);
-        analyze_window(window);
-        place(window, maximizeRect);
+        window.frameGeometry = maximizeRect;
+        //analyze_window(window);
+        //place(window, maximizeRect);
     }
 }
 
-function place(window, clientArea) {
-    dbg("Hey there. installed script is running!");
+function old_place(window, clientArea) {
+    dbg("Hey there. installed script with old parameters is running!");
     analyze_window(window);
     //dbg("clientArea in script: " + clientArea);
     //let area = Qt.rect(10, 20, 30, 40);
@@ -87,13 +140,10 @@ function place(window, clientArea) {
     //return "Blergh";
 }
 
-
-
-
 dbg("installed script mobilewindows is running.");
 
 workspace.setPlacementCallback(place);
 
-dbg("running init().");
+dbg("############## running init(). ##############################");
 
 init();
